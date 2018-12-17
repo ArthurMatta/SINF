17f30ecd256094d8b39893e739d15a331372c67e
Arthur26041996 <arthurmatta@hotmail.com>
2018-12-17 01:23:16 +0000

    initial implementation of sign up page.

--- myproject/src/components/auth/signup/Signup.vue
+++ myproject/src/components/auth/signup/Signup.vue
@@ -2,25 +2,100 @@
   <div class="signup">
     <h2>{{'auth.createNewAccount' | translate}}</h2>
     <form method="post" action="/auth/signup" name="signup">
-      <div class="form-group">
+      <div class="form-group with-icon-right"
+           :class="{'has-error': errors.has('name'), 'valid': isFormFieldValid('name')}">
         <div class="input-group">
-          <input type="text" id="email" required="required"/>
+          <input
+            type="text"
+            name="name"
+            v-model="name"
+            v-validate="'required|min:3|max:50|alpha_spaces'"
+            required="required"/>
+          <i
+            class="fa fa-exclamation-triangle error-icon icon-right input-icon"></i>
+          <i class="fa fa-check valid-icon icon-right input-icon"></i>
+          <label class="control-label">Name</label><i class="bar"></i>
+          <small v-show="errors.has('name')" class="help text-danger">{{
+            errors.first('name') }}
+          </small>
+        </div>
+      </div>
+      <div class="form-group with-icon-right"
+           :class="{'has-error': errors.has('email'), 'valid': isFormFieldValid('email')}">
+        <div class="input-group">
+          <input
+            type="text"
+            id="email"
+            name="email"
+            v-validate="'required|email'"
+            required="required"/>
+          <i
+            class="fa fa-exclamation-triangle icon-right input-icon"
+            v-show="errors.has('email')"></i>
+          <i class="fa fa-check valid-icon icon-right input-icon"></i>
           <label class="control-label" for="email">{{'auth.email' | translate}}</label><i class="bar"></i>
+          <small v-show="errors.has('email')" class="help text-danger">{{
+            errors.first('email') }}
+          </small>
         </div>
       </div>
-      <div class="form-group">
+      <div class="form-group with-icon-right"
+           :class="{'has-error': errors.has('phone'), 'valid': isFormFieldValid('phone')}">
         <div class="input-group">
-          <input type="password" id="password" required="required"/>
-          <label class="control-label" for="password">{{'auth.password' | translate}}</label><i class="bar"></i>
+          <input
+            type="text"
+            id="phone"
+            name="phone"
+            v-validate="'required|digits:9'"
+            required="required"/>
+          <i
+            class="fa fa-exclamation-triangle icon-right input-icon"
+            v-show="errors.has('phone')"></i>
+          <i class="fa fa-check valid-icon icon-right input-icon"></i>
+          <label class="control-label" for="phone">Phone number</label><i class="bar"></i>
+          <small v-show="errors.has('phone')" class="help text-danger">{{
+            errors.first('phone') }}
+          </small>
         </div>
       </div>
-      <vuestic-checkbox
-        :id="'checkbox1'"
-        v-model="checkboxOneModel">
-        <template slot="label">{{'auth.agree' | translate}}
-          <router-link to="">{{'auth.termsOfUse' | translate}}</router-link>
-        </template>
-      </vuestic-checkbox>
+      <div class="form-group with-icon-right"
+           :class="{'has-error': errors.has('password'), 'valid': isFormFieldValid('password')}">
+        <div class="input-group">
+          <input
+            type="password"
+            id="password"
+            name="password"
+            v-validate="'required|min:8|max:25'"
+            required="required"/>
+          <i
+            class="fa fa-exclamation-triangle icon-right input-icon"
+            v-show="errors.has('password')"></i>
+          <i class="fa fa-check valid-icon icon-right input-icon"></i>
+          <label class="control-label" for="password">{{'auth.password' | translate}}</label><i class="bar"></i>
+          <small v-show="errors.has('password')" class="help text-danger">{{
+            errors.first('password') }}
+          </small>
+        </div>
+      </div>
+      <div class="form-group with-icon-right"
+           :class="{'has-error': errors.has('termsOfUse'), 'valid': isFormFieldValid('termsOfUse')}">
+        <div class="input-group">
+          <vuestic-checkbox
+            :id="'termsOfUse'"
+            v-validate="'required'"
+            v-model="checkboxOneModel">
+            {{'auth.agree' | translate}}
+            <router-link to="/admin/dashboard">{{'auth.termsOfUse' | translate}}</router-link>
+          </vuestic-checkbox>
+          <i
+            class="fa fa-exclamation-triangle icon-right input-icon"
+            v-show="errors.has('termsOfUse')"></i>
+          <i class="fa fa-check valid-icon icon-right input-icon"></i>
+          <small v-show="errors.has('termsOfUse')" class="help text-danger">{{
+            errors.first('termsOfUse') }}
+          </small>
+        </div>
+      </div>
       <div class="d-flex flex-column flex-lg-row align-items-center justify-content-between down-container">
         <button class="btn btn-primary" type="submit">
           {{'auth.signUp' | translate}}
@@ -36,7 +111,19 @@ export default {
   name: 'signup',
   data () {
     return {
-      checkboxOneModel: true
+      checkboxOneModel: false
+    }
+  },
+  methods: {
+    isFormFieldValid (field) {
+      let isValid = false
+      if (this.formFields[field]) {
+        isValid = this.formFields[field].validated && this.formFields[field].valid
+      }
+      return isValid
+    },
+    validateFormField (fieldName) {
+      this.$validator.validate(fieldName, this[fieldName])
     }
   }
 }
