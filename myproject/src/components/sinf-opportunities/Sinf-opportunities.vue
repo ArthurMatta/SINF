ba3b454cf12d2e825c40f34f15d1c8688315bda7
Vitor Esteves <up201303104@fe.up.pt>
2018-12-19 04:09:25 +0000

    form updated

--- myproject/src/components/sinf-opportunities/Sinf-opportunities.vue
+++ myproject/src/components/sinf-opportunities/Sinf-opportunities.vue
@@ -1,130 +1,115 @@
 <template>
-  <div class="sinf-opportunities">
-   <div class="form-elements">
-    <div class="row">
-      <div class="col-md-12">
-        <vuestic-widget :headerText="'forms.inputs.title' | translate">
-          <form>
+	<div class="sinf-opportunities">
+		<div class="form-elements">
+			<div class="row">
+				<div class="col-md-12">
+					<vuestic-widget :headerText="'forms.inputs.title' | translate">
+						<form>
 
-            <div class="row">
-              <div class="col-md-4">
-                <fieldset>
-                  <div class="form-group">
-                    <div class="input-group">
-                      <input id="simple-input" required/>
-                      <label class="control-label" for="simple-input">{{'forms.inputs.textInput'
-                        | translate}}</label><i class="bar"></i>
-                    </div>
-                  </div>
-                </fieldset>
-              </div>
-              <div class="col-md-4">
-                <fieldset>
-                  <div class="form-group">
-                    <div class="input-group">
-                      <input id="simple-input" required/>
-                      <label class="control-label" for="simple-input">{{'forms.inputs.textInput'
-                        | translate}}</label><i class="bar"></i>
-                    </div>
-                  </div>
-                </fieldset>
-              </div>
-              <div class="col-md-4">
-                <fieldset>
-                  <div class="form-group">
-                    <div class="input-group">
-                      <input id="simple-input" required/>
-                      <label class="control-label" for="simple-input">{{'forms.inputs.textInput'
-                        | translate}}</label><i class="bar"></i>
-                    </div>
-                  </div>
-                </fieldset>
-              </div>
-             </div>
-              <div class="row">
-              <div class="col-md-4">
-                <fieldset>
-                  <div class="form-group">
-                    <div class="input-group">
-                      <input id="simple-input" required/>
-                      <label class="control-label" for="simple-input">{{'forms.inputs.textInput'
-                        | translate}}</label><i class="bar"></i>
-                    </div>
-                  </div>
-                </fieldset>
-              </div>
-              <div class="col-md-4">
-                <fieldset>
-                  <div class="form-group">
-                    <div class="input-group">
-                      <input id="simple-input" required/>
-                      <label class="control-label" for="simple-input">{{'forms.inputs.textInput'
-                        | translate}}</label><i class="bar"></i>
-                    </div>
-                  </div>
-                </fieldset>
-              </div>
-              <div class="col-md-4">
-                <fieldset>
-                  <div class="form-group">
-                    <div class="input-group">
-                      <input id="simple-input" required/>
-                      <label class="control-label" for="simple-input">{{'forms.inputs.textInput'
-                        | translate}}</label><i class="bar"></i>
-                    </div>
-                  </div>
-                </fieldset>
-              </div>
-             </div>
-              <div class="row">
-              <div class="col-md-4">
-                <fieldset>
-                  <div class="form-group">
-                    <div class="input-group">
-                      <input id="simple-input" required/>
-                      <label class="control-label" for="simple-input">{{'forms.inputs.textInput'
-                        | translate}}</label><i class="bar"></i>
-                    </div>
-                  </div>
-                </fieldset>
-              </div>
-              <div class="col-md-4">
-                <fieldset>
-                  <div class="form-group">
-                    <div class="input-group">
-                      <input id="simple-input" required/>
-                      <label class="control-label" for="simple-input">{{'forms.inputs.textInput'
-                        | translate}}</label><i class="bar"></i>
-                    </div>
-                  </div>
-                </fieldset>
-                <div class="d-flex justify-content-center">
-                <button class="btn btn-primary">
-                {{'buttons.button' | translate}}
-              </button>
-              </div>
-              </div>
-             </div>
-        </form>
-        </vuestic-widget>
-      </div>
-     </div>
-     </div>
-     </div>
-</template>
+							<div class="row">
+								<div class="col-md-4">
+									<fieldset>
+										<div class="form-group">
+											<div class="input-group">
+												<input id="simple-input" required/>
+												<label class="control-label" for="simple-input">{{'ID'
+													| translate}}</label><i class="bar"></i>
+												</div>
+											</div>
+										</fieldset>
+									</div>
+									<div class="col-md-4">
+										<fieldset>
+											<div class="form-group">
+												<div class="input-group">
+													<input id="simple-input" required/>
+													<label class="control-label" for="simple-input">{{'Name'
+														| translate}}</label><i class="bar"></i>
+													</div>
+												</div>
+											</fieldset>
+										</div>
+										<div class="col-md-4">
+											<fieldset>
+												<div class="form-group">
+													<div class="input-group">
+														<input id="simple-input" required/>
+														<label class="control-label" for="simple-input">{{'Adress'
+															| translate}}</label><i class="bar"></i>
+														</div>
+													</div>
+												</fieldset>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-4">
+												<fieldset>
+													<div class="form-group">
+														<div class="input-group">
+															<input id="simple-input" required/>
+															<label class="control-label" for="simple-input">{{'Phone'
+																| translate}}</label><i class="bar"></i>
+															</div>
+														</div>
+													</fieldset>
+												</div>
 
-<script>
-  import GoogleMapsPage from '../maps/google-maps/GoogleMapsPage'
-  import GoogleMap from '../maps/google-maps/GoogleMap'
+											<div class="col-md-4">
+												<fieldset>
+													<vuestic-simple-select
+													:label="'Select Salesman' | translate"
+													v-model="simpleSelectModel"
+													option-key="description"
+													v-bind:options="simpleOptions"
+													/>
+												</fieldset>
+												</div>
+											</div>
 
-  export default {
-    components:{
-      GoogleMapsPage,
-      GoogleMap
-    }
-  }
-</script>
+											<div class="d-flex justify-content-center">
+												<button class="btn btn-primary">
+													{{'buttons.button' | translate}}
+												</button>
+											</div>
+										</form>
+									</vuestic-widget>
+								</div>
+							</div>
+						</div>
+					</div>
+				</template>
 
-<style scoped>
+				<script>
+					import GoogleMapsPage from '../maps/google-maps/GoogleMapsPage'
+					import GoogleMap from '../maps/google-maps/GoogleMap'
 
-</style>
+					export default {
+						components:{
+							GoogleMapsPage,
+							GoogleMap
+						},
+						data () {
+							return {
+								simpleOptions: [
+								{
+									id: 1,
+									description: 'First option',
+								},
+								{
+									id: 2,
+									description: 'Second option',
+								},
+								{
+									id: 3,
+									description: 'Third option',
+								},
+								],
+								simpleSelectModel: ''
+							}
+						}
+					}
+				</script>
+
+				<style scoped>
+
+				</style>
