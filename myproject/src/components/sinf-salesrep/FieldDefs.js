a17db4c813eacb989238e8e4ee49ef75c2f0d380
Arthur26041996 <arthurmatta@hotmail.com>
2018-12-19 05:17:15 +0000

    Initial implementation of sales representative page

---
+++ myproject/src/components/sinf-salesrep/FieldDefs.js
@@ -0,0 +1,9 @@
+export default [
+  {
+    name: 'title',
+    sortField: 'title'
+  },
+  {
+    name: 'descriptiom',
+  }
+]
--- myproject/src/components/sinf-salesrep/Sinf-salesrep.vue
+++ myproject/src/components/sinf-salesrep/Sinf-salesrep.vue
@@ -1,20 +1,103 @@
 <template>
-  <div>
+  <div class="extra">
     <div class="row">
-      <div class="col-xs-12 col-md-12">
-        <vuestic-widget :headerText="$t('tables.basic')">
-
+      <div class="col-md-4 d-flex">
+          <vuestic-widget :headerText="$t('My Profile')" class="profile-card-widget">
+          <vuestic-profile-card :name="'Veronique Lee'" :location="'Malaga, Spain'" photoSource="https://i.imgur.com/NLrdqsk.png"
+                                :social="{twitter: 'twitter.com', facebook: 'facebook.com',
+                                  instagram: 'instagram.com'}"
+                                class="w-100"
+          >
+          </vuestic-profile-card>
+        </vuestic-widget>
+      </div>
+      <div class="col-md-8">
+        <vuestic-widget :headerText="'extra.tabs.title' | translate" class="no-v-padding">
+          <vuestic-tabs class="tabs" :names="[$t('Statistics'), $t('Tasks'), $t('extra.tabs.maps')]">
+            <div :slot="'Statistics' | translate" class="d-flex justify-content-center">
+              <data-visualization-tab></data-visualization-tab>
+            </div>
+            <div :slot="'extra.tabs.maps' | translate" class="maps-tab">
+              <leaflet-map></leaflet-map>
+            </div>
+            <div :slot="'Tasks' | translate">
+              <deals-table
+                api-url="https://vuetable.ratiw.net/api/users"
+                :fields="fields"
+                :sort-order="sortOrder"
+                :append-params="moreParams"
+              >
+              </deals-table>
+            </div>
+          </vuestic-tabs>
         </vuestic-widget>
       </div>
     </div>
   </div>
 </template>
 
-<style lang="scss">
+<script>
+  import LeafletMap from 'components/maps/leaflet-maps/LeafletMap.vue'
+  import DataVisualizationTab from '../sinf-dashboard/data-visualisation-tab/DataVisualisation'
+  import VuesticWidget from "../../vuestic-theme/vuestic-components/vuestic-widget/VuesticWidget";
+  import DealsTable from '../sinf-salesorders/DealsTable'
+  import FieldsDefs from './FieldDefs'
 
-  .color-icon-label-table {
-    td:first-child {
-      width: 1rem;
+  export default {
+    name: 'extra',
+    components: {
+      VuesticWidget,
+      LeafletMap,
+      DataVisualizationTab,
+      DealsTable
+    },
+    data () {
+      return {
+        apiUrl: 'https://vuetable.ratiw.net/api/users',
+        fields: FieldsDefs,
+        sortOrder: [
+          {
+            field: '__slot:name',
+            sortField: 'name',
+            direction: 'asc'
+          }
+        ],
+        moreParams: {}
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .tabs {
+    .overview-tab {
+      .explore-row {
+        display: none !important;
+      }
+    }
+
+    .maps-tab {
+      height: 500px;
+    }
+  }
+
+  .profile-card-widget, .chat-widget {
+    width: 100%;
+    .widget-body {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      & > div {
+        width: 100%;
+      }
+    }
+  }
+
+  .bottom-widgets {
+    > div[class^='col'] {
+      & > div {
+        width: 100%;
+      }
     }
   }
 </style>
